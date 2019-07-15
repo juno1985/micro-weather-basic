21b2bf88a0d4c238101c941b7f2babcdbfe986b0
Jun <wangzhen_tju@126.com>
2019-07-15 15:00:33 +0800

    Quartz配置(JobDetail & Trigger)

---
+++ src/main/java/com/juno/spring/cloud/weather/config/QuarzConfiguration.java
@@ -0,0 +1,30 @@
+package com.juno.spring.cloud.weather.config;
+
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.juno.spring.cloud.weather.job.WeatherDataSyncJob;
+
+@Configuration
+public class QuarzConfiguration {
+
+	//JobDetail
+	@Bean
+	public JobDetail weatherDataSyncJobDetail() {
+		return JobBuilder.newJob(WeatherDataSyncJob.class)
+		.withIdentity("WeatherDataSyncJob").storeDurably().build();
+	}
+	//Trigger
+	@Bean
+	public Trigger weatherDataSyncTrigger() {
+		//每两秒执行
+		SimpleScheduleBuilder schedBuilder = SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(2).repeatForever();
+		return TriggerBuilder.newTrigger().forJob(weatherDataSyncJobDetail())
+				.withIdentity("weatherDataSyncTrigger").withSchedule(schedBuilder).build();
+	}
+}
---
+++ src/main/java/com/juno/spring/cloud/weather/job/WeatherDataSyncJob.java
@@ -0,0 +1,18 @@
+package com.juno.spring.cloud.weather.job;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class WeatherDataSyncJob extends QuartzJobBean {
+	
+	private final static Logger logger = LoggerFactory.getLogger(WeatherDataSyncJob.class);
+
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		logger.info("Quartz Job Triggered!");
+	}
+
+}
