23609843351ffb048c8aecc4dbd32a6376471dd5
Jun <wangzhen_tju@126.com>
2019-07-15 14:22:20 +0800

    redis缓冲数据

--- src/main/java/com/juno/spring/cloud/weather/service/WeatherDataServiceImpl.java
+++ src/main/java/com/juno/spring/cloud/weather/service/WeatherDataServiceImpl.java
@@ -1,23 +1,31 @@
 package com.juno.spring.cloud.weather.service;
 
-import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.juno.spring.cloud.weather.vo.WeatherResponse;
 @Service
 public class WeatherDataServiceImpl implements WeatherDataService {
+	
+	private static Logger logger = LoggerFactory.getLogger(WeatherDataServiceImpl.class);
+	//set time out for redis
+	private final long timeout = 1800L;
 
 	private static final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini?";
 
 	@Autowired
 	private RestTemplate restTemplate;
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate; 
 
 	@Override
 	public WeatherResponse getDataByCityId(String cityId) {
@@ -34,22 +42,32 @@ public class WeatherDataServiceImpl implements WeatherDataService {
 	}
 
 	private WeatherResponse doGetWeather(String uri) {
-		ResponseEntity<String> wresp = restTemplate.getForEntity(uri, String.class);
-		String respStr = wresp.getBody();
-		ObjectMapper jsonMapper = new ObjectMapper();
 		
+		ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+		String key = uri;
 		WeatherResponse resp  = null;
-		if (wresp.getStatusCodeValue() == 200) {
-			try {
-				resp = jsonMapper.readValue(respStr, WeatherResponse.class);
-			} catch (JsonParseException e) {
-				e.printStackTrace();
-			} catch (JsonMappingException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
+		String respStr = null;
+		if(stringRedisTemplate.hasKey(key)) {
+			respStr = ops.get(key);
+			logger.info("Hits Redis!");
+		}else {
+			ResponseEntity<String> wresp = restTemplate.getForEntity(uri, String.class);
+			if (wresp.getStatusCodeValue() != 200) {
+				return null;
 			}
+			respStr = wresp.getBody();
+			logger.info("Miss hitting Redis");
+			
+			ops.set(key, respStr, timeout, TimeUnit.SECONDS);
 		}
+		
+		try {
+			ObjectMapper jsonMapper = new ObjectMapper();
+			resp = jsonMapper.readValue(respStr, WeatherResponse.class);
+		} catch (Exception e) {
+			logger.error("Error","Json to Object Conversion Error");
+		} 
+		
 		return resp;
 	}
 
